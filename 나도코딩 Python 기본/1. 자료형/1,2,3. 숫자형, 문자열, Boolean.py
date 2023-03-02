8b157c534d8cfc4965dac06d15b9ffd74e2de7a1
jhy <yuns_97@naver.com>
2023-03-02 19:10:11 +0900

    Section 1. 자료형

---
+++ 나도코딩 Python 기본/1. 자료형/1,2,3. 숫자형, 문자열, Boolean.py
@@ -0,0 +1,23 @@
+# 1. 숫자 자료형
+print(5)
+print(-10)
+print(3.14)
+print(1000)
+print(5+3)
+print(2*8)
+print(3*(3+1))
+
+# 2. 문자열 자료형
+print('풍선')
+print("나비")
+print("ㅋㅋㅋㅋㅋㅋㅋㅋㅋ")
+print("ㅋ"*9)
+
+# 3. boolean (참/거짓) 자료형
+print(5 > 10)
+print(5 < 10)
+print(True)
+print(False)
+print(not True)
+print(not False)
+print(not (5 > 10))
---
+++ 나도코딩 Python 기본/1. 자료형/Quiz 1.py
@@ -0,0 +1,8 @@
+# Quiz 1. 변수를 이용하여 다음 문장을 출력하시오.
+
+station = "사당"
+print(station + " 행 열차가 들어오고 있습니다.")
+station = "신도림"
+print(station + " 행 열차가 들어오고 있습니다.")
+station = "인천공항"
+print(station + " 행 열차가 들어오고 있습니다.")
